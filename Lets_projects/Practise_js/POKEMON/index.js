7964a1068031939516352dd28fc7c52f23f8f8d6
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2021-10-04 22:04:03 +0530

    Upadated JS work.

--- Lets_projects/Practise_js/POKEMON/index.js
+++ Lets_projects/Practise_js/POKEMON/index.js
@@ -1,9 +1,28 @@
-fetch("https://lu-poke-api.herokuapp.com/pokemons")
+// display all the pokemons
+display=(pokemons)=>{
+    let pokeStrings = "";
+    pokemons.map((pokemons,index)=>{
+        pokeStrings += `
+        
+             <div class="pokemon-content">
+                <div class="content">
+                    <h2>${pokemons.name}</h2>
+                    <p>${pokemons.type}</p>
+                </div>
+            </div>
+        `
+})
+document.getElementById('pokemons').innerHTML = pokeStrings;
+}
+
+// fetch all pokemons
+fetch("http://localhost:8000/pokemons")
 .then((response)=>{
     return response.json();
 })
 .then((data)=>{
     console.log(data);
+    display(data);
 })
 .catch((err)=>{
     console.log(err);
